3c691f931c1a05b48212636e863fae0e634426ef
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-25 17:21:19 -0300

    Condicionais
    exemplo de condicionais para quem esta iniciando na programação

---
+++ condicionais/if/Program.cs
@@ -0,0 +1,15 @@
+﻿
+DateTime anoNascimento = new DateTime(1987, 04, 11);
+DateTime hoje = DateTime.Today;
+
+int idade = (hoje.Year - anoNascimento.Year);
+
+
+
+if (idade < 18 || idade > 65)
+{
+    Console.WriteLine($"Com {idade} anos, não é permitido trabalhar aqui");
+} else
+{
+    Console.WriteLine("Bao sorte no processo");
+}
---
+++ condicionais/switch/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Modo tradicional
+
+int data = new DateTime(1987, 04, 01).Month;
+
+switch (data)
+{
+    case < 3:
+        Console.WriteLine("Verão");
+            break;
+    case < 6:
+        Console.WriteLine("Outono");
+            break;
+    case < 9:
+        Console.WriteLine("Inverno");
+            break;
+    case < 12:
+        Console.WriteLine("Verão");
+            break;
+    default: Console.WriteLine("Erro");
+        break;
+}
+
+
+
+// ou... Usando Arrow Function (Lambda)
+
+
+static string EstacaoDoAno(DateTime data) => data.Month switch
+{
+    >= 3 and <= 6 => "Outono",
+    >= 6 and <= 9 => "inverno",
+    >= 9 and <= 12 => "Primavera",
+    12 or (>=1 and < 3) => "Verão",
+    _ => "Erro"
+};
+Console.WriteLine(EstacaoDoAno(DateTime.Now));
